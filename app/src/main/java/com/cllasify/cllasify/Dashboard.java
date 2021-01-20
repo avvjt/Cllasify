6b5de8d09c7c73d1beebf0ac363894f2c67f4441
ABHI <featabhijit@gmail.com>
2021-01-20 11:04:08 -0800

    bottom navigation bar add

--- app/src/main/java/com/cllasify/cllasify/Dashboard.java
+++ app/src/main/java/com/cllasify/cllasify/Dashboard.java
@@ -3,12 +3,49 @@ package com.cllasify.cllasify;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class Dashboard extends AppCompatActivity {
 
+    ChipNavigationBar chipNavigationBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        chipNavigationBar = findViewById(R.id.bottom_nav_menu);
+        chipNavigationBar.setItemSelected(R.id.bottom_nav_home,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+        bottomMenu();
+    }
+
+    private void bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i) {
+                    case R.id.bottom_nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.bottom_nav_friends:
+                        fragment = new FriendsFragment();
+                        break;
+                    case R.id.bottom_nav_feed:
+                        fragment = new FeedFragment();
+                        break;
+                    case R.id.bottom_nav_notification:
+                        fragment = new NotificationFragment();
+                        break;
+                    case R.id.bottom_nav_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            }
+        });
+
     }
 }
